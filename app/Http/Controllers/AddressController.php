ca074310961be19a488d4bf7b771ec037ff42265
Андрей Винокуров <andrusha.vinokurov@gmail.com>
2022-07-08 16:26:49 +0700

    Test PgbdAdress

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\AddressModel;
 use Illuminate\Support\Facades\Storage;
+use Nette\Utils\Json;
 
 class AddressController extends Controller
 {
     public function foo(Request $request)
     {
         $model = new AddressModel();
-        $model->title = Storage::disk('public')->put('movies.json', response()->json($request->input("address")));
-        $model->save();
-        return response()->json(["message"=>"okey"],200);
+        
+        //Storage::disk('public')->put('movies.json', response()->json($request->input("address")));
+        //$model->save();
+        return response()->json($model::all(),200);
     }
 }
