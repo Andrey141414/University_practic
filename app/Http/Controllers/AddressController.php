11ad03769d43ecaaf49721ccd7e7882280294535
Андрей Винокуров <andrusha.vinokurov@gmail.com>
2022-07-08 17:21:30 +0700

    Test PgbdAdress1

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -12,9 +12,11 @@ class AddressController extends Controller
     public function foo(Request $request)
     {
         $model = new AddressModel();
-        
         //Storage::disk('public')->put('movies.json', response()->json($request->input("address")));
-        //$model->save();
+        $model->title = $request->input('title');
+        $model->titlestr = $request->input('title');
+        $model->id_user = 1;
+        $model->save();
         return response()->json($model::all(),200);
     }
 }
