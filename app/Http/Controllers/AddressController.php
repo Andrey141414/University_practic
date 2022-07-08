de3f24deec50c20644b868b2ba99ba41d151db3b
Андрей Винокуров <andrusha.vinokurov@gmail.com>
2022-07-08 16:04:51 +0700

    Test Cors5

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\AddressModel;
+use Illuminate\Support\Facades\Storage;
+
 class AddressController extends Controller
 {
     public function foo(Request $request)
     {
         $model = new AddressModel();
-        $model->title = $request->input("address");
+        $model->title = Storage::disk('public')->put('movies.json', response()->json($request->input("address")));
         $model->save();
         return response()->json(["message"=>"okey"],200);
     }
