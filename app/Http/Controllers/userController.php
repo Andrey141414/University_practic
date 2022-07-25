e0615792c091de1ae8f4915cf37cc92562cb16f2
Андрей Винокуров <andrusha.vinokurov@gmail.com>
2022-07-25 17:34:08 +0700

    Test photos6

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -53,9 +53,9 @@ class userController extends Controller
 
         // } 
 
-        Storage::disk("local")->deleteDirectory('IN_GOOD_HANDS');
+        //Storage::disk("local")->deleteDirectory('IN_GOOD_HANDS');
         $path = 'IN_GOOD_HANDS/12/111';
-        $a = Storage::disk("google")->exists($path.'/0.jpeg');
+        $a = Storage::disk("local")->exists($path.'/0.jpeg');
         return response()->json($a);
 
         // $items_num = 3;
