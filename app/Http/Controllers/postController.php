ac4588944253e12fbc05ff05f79e0337ea4bd888
Андрей Винокуров <andrusha.vinokurov@gmail.com>
2022-07-25 18:58:22 +0700

    Test photos12

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -185,7 +185,7 @@ class postController extends Controller
         }
          
         
-        return [(new postModel())->simplePaginate(5),$previews];
+        return [(new postModel())->all(),$previews];
     
     }
 
